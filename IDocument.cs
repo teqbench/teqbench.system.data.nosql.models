e92015ed76727469e78e9ccd6c83e5cf771fe5cc
Ben Visintainer <ben.visintainer@me.com>
2023-08-25 17:27:45 -0600

    Initial checkin; restructured root folder and name of solution/project files for easier navigation.

---
+++ IDocument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TradingToolbox.System.Data.NoSql.Models
+{
+    /// <summary>
+    /// Base interface for a repository document model.
+    /// </summary>
+    /// <typeparam name="T">The instance type (i.e. domain model document type) of this document instance.</typeparam>
+    public interface IDocument<T>
+    {
+        /// <summary>
+        /// Unique identifier of document. This will generally be auto-generated when document is created in DB.
+        /// </summary>
+        T Id { get; set; }
+
+        /// <summary>
+        /// Creatation date/time of document instance.
+        /// </summary>
+        DateTime CreatedAt { get; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Overview
+Base interface for a repository document model.
+
+Keep as a separate package/lib as to NOT also have to include/reference TradingToolbox.System.Data.NoSql.Repository when implementing models based on this interface.
+# Build
+# Deployment
