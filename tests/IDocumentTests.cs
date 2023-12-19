f1440d8381c23fb45168b1871f5577620af9c6ef
Ben <ben.visintainer@me.com>
2023-12-18 22:47:57 -0700

    Added unit tests. Reorganized solution to better accommodate multiple projects in solution. Added editor config file.

---
+++ tests/IDocumentTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+
+namespace TeqBench.System.Data.NoSql.Models.Tests;
+
+[TestClass]
+public class IDocumentTests
+{
+    [TestMethod]
+    public void TestPropertyStringDefaults()
+    {
+        var mock = new Mock<IDocument<string>>();
+
+        IDocument<string> doc = mock.Object;
+        Assert.IsNull(doc.Id);
+        Assert.AreEqual(doc.CreatedAt, DateTime.MinValue);
+    }
+
+    [TestMethod]
+    public void TestPropertyIntDefaults()
+    {
+        var mock = new Mock<IDocument<int>>();
+
+        IDocument<int> doc = mock.Object;
+        Assert.AreEqual(doc.Id, 0);
+        Assert.AreEqual(doc.CreatedAt, DateTime.MinValue);
+    }
+
+    [TestMethod]
+    public void TestIdAssignment()
+    {
+        var mock = new Mock<IDocument<string>>();
+        mock.SetupProperty(doc => doc.Id);
+
+        IDocument<string> doc = mock.Object;
+        doc.Id = "a doc ID";
+        Assert.AreEqual(doc.Id, "a doc ID");
+    }
+}
